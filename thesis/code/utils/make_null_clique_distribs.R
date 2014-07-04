acd7fb7845d98b41211f0e16fdf1d8e898a8b121
Ted Natoli <ted.e.natoli@gmail.com>
2014-07-03 20:29:38 -0400

    added iteration through scores

--- thesis/code/utils/make_null_clique_distribs.R
+++ thesis/code/utils/make_null_clique_distribs.R
@@ -10,16 +10,21 @@ require("igraph", lib.loc="/Users/tnatoli/github/thesis/thesis/code/R")
 args <- commandArgs(trailingOnly=T)
 space_file <- args[1]
 outpath <- args[2]
-score_thresh <- args[3]
-max_sample_size <- args[4]
-min_size <- args[5]
+min_score <- args[3]
+max_score <- args[4]
+max_sample_size <- args[5]
+min_size <- args[6]
 
-if(is.na(score_thresh)) {
-	score_thresh <- 90
+if(is.na(min_score)) {
+	min_score <- 90
+}
+
+if(is.na(max_score)) {
+	max_score <- 99
 }
 
 if(is.na(max_sample_size)) {
-	max_sample_size <- 10
+	max_sample_size <- 20
 }
 
 if(is.na(min_size)) {
@@ -47,26 +52,31 @@ cat("iterating...\n")
 for (i in 2:max_sample_size) {
 	cat(paste(i, "\t"))
 	# for every sample size
-	for(j in 1:100) {
-		cat(paste("\t", j, "\n"))
+	for(j in 1:1000) {
+		cat(paste("\t", j, "\t"))
 		# for 1000 iterations
-		sample_space <- sample(nodes, i)
-		samp <- droplevels(subset(d, pert_iname_x %in% sample_space & pert_iname_y %in% sample_space))
-		g <- graph.data.frame(samp, directed=F)
-		graph_cliques <- cliques(g, min=min_size)
-		num_cliques <- length(graph_cliques)
-		largest_cliques <- largest.cliques(g)
-		if(length(largest_cliques) != 0) {
-			largest_clique_size <- length(largest_cliques[[1]])
+		for(z in min_score:max_score) {
+			cat(paste("\t", "\t", z, "\n"))
+			# for every score
+			sample_space <- sample(nodes, i)
+			samp <- droplevels(subset(d, pert_iname_x %in% sample_space & pert_iname_y %in% sample_space))
+			g <- graph.data.frame(samp, directed=F)
+			graph_cliques <- cliques(g, min=min_size)
+			num_cliques <- length(graph_cliques)
+			largest_cliques <- largest.cliques(g)
+			if(length(largest_cliques) != 0) {
+				largest_clique_size <- length(largest_cliques[[1]])
+			}
+			else {
+				largest_clique_size <- 0
+			}
+			tmp <- data.frame(sample_size=i,
+							  iteration=j,
+							  score=z,
+							  num_cliques=num_cliques,
+							  largest_clique_size=largest_clique_size)
+			dlist[[length(dlist) + 1]] <- tmp
 		}
-		else {
-			largest_clique_size <- 0
-		}
-		tmp <- data.frame(sample_size=i,
-						  iteration=j,
-						  num_cliques=num_cliques,
-						  largest_clique_size=largest_clique_size)
-		dlist[[length(dlist) + 1]] <- tmp
 	}
 }
 
