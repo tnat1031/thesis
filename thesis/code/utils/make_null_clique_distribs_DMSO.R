4ec4dfb27c6300e379e1fdf846158dd734c515f8
Ted Natoli <ted.e.natoli@gmail.com>
2014-07-13 20:17:16 -0400

    run one job per signature

--- thesis/code/utils/make_null_clique_distribs_DMSO.R
+++ thesis/code/utils/make_null_clique_distribs_DMSO.R
@@ -10,12 +10,13 @@ require("igraph", lib.loc="/Users/tnatoli/github/thesis/thesis/code/R")
 args <- commandArgs(trailingOnly=T)
 space_file <- args[1]
 summly_file <- args[2]
-outpath <- args[3]
-min_score <- args[4]
-max_score <- args[5]
-min_sample_size <- args[6]
-max_sample_size <- args[7]
-min_clique_size <- args[8]
+sig_id <- args[3]
+outpath <- args[4]
+min_score <- args[5]
+max_score <- args[6]
+min_sample_size <- args[7]
+max_sample_size <- args[8]
+min_clique_size <- args[9]
 
 if(is.na(min_score)) {
 	min_score <- 90
@@ -38,52 +39,47 @@ if(is.na(min_clique_size)) {
 }
 
 # define main function
-get_cliques <- function(min_sample_size, max_sample_size, min_score, max_score) {
+get_cliques <- function(sig_id, min_sample_size, max_sample_size, min_score, max_score) {
 	dlist <- list()
 	cat("iterating...\n")
 	for (i in min_sample_size:max_sample_size) {
 		cat(paste(i, "\t"))
-		# for every signature	
-		for(j in levels(as.factor(d$id_y))) {
-			cat(paste("\t", j, "\t"))
-			# for 1000 iterations
-			for(z in min_score:max_score) {
-				cat(paste("\t", "\t", z, "\n"))
-				# for every score
-				# subset to connection threshold
-				cat("subsetting...\n")
-				dsub <- droplevels(subset(d, abs(score) >= z & id_y == j))
-				dsub <- droplevels(dsub[, c("pert_iname_x", "pert_iname_y")])
-				
-				# get list of all nodes in dataset
-				cat("generating list of nodes...\n")
-				nodes <- unique(c(as.character(dsub$pert_iname_x), as.character(dsub$pert_iname_y)))
-				sample_space <- sample(nodes, i)
-				
-				# look up their scores in summly space
-				cat("looking up scores...\n")
-				summly_sub <- droplevels(subset(summly, pert_iname_x %in% sample_space & pert_iname_y %in% sample_space,
-											select=c("pert_iname_x", "pert_iname_y")))
-				
-				# generate the graph anc compute some stats
-				cat("generating graph...\n")
-				g <- graph.data.frame(summly_sub, directed=F)
-				graph_cliques <- cliques(g, min=min_clique_size)
-				num_cliques <- length(graph_cliques)
-				largest_cliques <- largest.cliques(g)
-				if(length(largest_cliques) != 0) {
-					largest_clique_size <- length(largest_cliques[[1]])
-				}
-				else {
-					largest_clique_size <- 0
-				}
-				tmp <- data.frame(sample_size=i,
-								  iteration=j,
-								  score=z,
-								  num_cliques=num_cliques,
-								  largest_clique_size=largest_clique_size)
-				dlist[[length(dlist) + 1]] <- tmp
+		for(z in min_score:max_score) {
+			cat(paste("\t", "\t", z, "\n"))
+			# for every score
+			# subset to connection threshold
+			cat("subsetting...\n")
+			dsub <- droplevels(subset(d, abs(score) >= z & id_y == sig_id))
+			dsub <- droplevels(dsub[, c("pert_iname_x", "pert_iname_y")])
+			
+			# get list of all nodes in dataset
+			cat("generating list of nodes...\n")
+			nodes <- unique(c(as.character(dsub$pert_iname_x), as.character(dsub$pert_iname_y)))
+			sample_space <- sample(nodes, i)
+			
+			# look up their scores in summly space
+			cat("looking up scores...\n")
+			summly_sub <- droplevels(subset(summly, pert_iname_x %in% sample_space & pert_iname_y %in% sample_space,
+										select=c("pert_iname_x", "pert_iname_y")))
+			
+			# generate the graph anc compute some stats
+			cat("generating graph...\n")
+			g <- graph.data.frame(summly_sub, directed=F)
+			graph_cliques <- cliques(g, min=min_clique_size)
+			num_cliques <- length(graph_cliques)
+			largest_cliques <- largest.cliques(g)
+			if(length(largest_cliques) != 0) {
+				largest_clique_size <- length(largest_cliques[[1]])
 			}
+			else {
+				largest_clique_size <- 0
+			}
+			tmp <- data.frame(sample_size=i,
+							  iteration=j,
+							  score=z,
+							  num_cliques=num_cliques,
+							  largest_clique_size=largest_clique_size)
+			dlist[[length(dlist) + 1]] <- tmp
 		}
 	}
 
