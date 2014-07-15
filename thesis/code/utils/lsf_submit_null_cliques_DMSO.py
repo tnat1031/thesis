d9779f6c3fb2fee97160f6d9672ad451ae487785
Ted Natoli <ted.e.natoli@gmail.com>
2014-07-14 20:09:21 -0400

    small fix

--- thesis/code/utils/lsf_submit_null_cliques_DMSO.py
+++ thesis/code/utils/lsf_submit_null_cliques_DMSO.py
@@ -4,6 +4,7 @@ simple helper script to submit null clique jobs to lsf
 
 import os
 import sys
+import cmap.io.plategrp as grp
 
 if __name__ == '__main__':
 	rscript_file = sys.argv[1]
@@ -17,14 +18,17 @@ if __name__ == '__main__':
 	min_score = sys.argv[9]
 	max_score = sys.argv[10]
 
-	# loop through scores and sample sizes
-	for size in range(min_size, max_size + 1):
-		for score in range(min_score, max_score + 1):
-			dirname = 'null_cliques_size_' + str(size) + '_score_' + str(score)
-			odir = os.path.join(outpath, dirname)
-			if not os.path.exists(odir):
-				os.mkdir(odir)
-			# Rscript code/utils/make_null_clique_distribs.R data/matched_lass_n10644x7533.txt ./ 90 90 5 5
-			cmd = 'bsub -P cmap -J dmso_{0}_{1} -o {2} -q week -R rusage[mem=4] Rscript {3} {4} {5} {6} {7} {8} {9} {10} {11}'.format(score, size, os.path.join(odir, 'lsf.out'), rscript_file, space_file, summly_file, sig_id_file, odir, score, score, size, size)
-			print cmd
-			#os.system(cmd)
+	sig_id_files = grp.read_grp(list_of_sig_ids)
+
+	# loop through scores and sample sizes and sig_id files
+	for s in sig_id_files:
+		for size in range(min_size, max_size + 1):
+			for score in range(min_score, max_score + 1):
+				dirname = 'null_cliques_size_' + str(size) + '_score_' + str(score)
+				odir = os.path.join(outpath, dirname)
+				if not os.path.exists(odir):
+					os.mkdir(odir)
+				# Rscript code/utils/make_null_clique_distribs.R data/matched_lass_n10644x7533.txt ./ 90 90 5 5
+				cmd = 'bsub -P cmap -J dmso_{0}_{1} -o {2} -q week -R rusage[mem=4] Rscript {3} {4} {5} {6} {7} {8} {9} {10} {11}'.format(score, size, os.path.join(odir, 'lsf.out'), rscript_file, space_file, summly_file, os.path.join(sig_id_file_path, s), odir, score, score, size, size)
+				print cmd
+				#os.system(cmd)
