3e28c42fc9cb1fe074fc7ec4d1277680b1bdae71
Ted Natoli <ted.e.natoli@gmail.com>
2014-08-07 08:14:20 -0400

    new analysis

--- thesis/code/utils/analyze_null_cliques.R
+++ thesis/code/utils/analyze_null_cliques.R
@@ -15,22 +15,31 @@ require("plyr")
 # get command line arguments
 args <- commandArgs(trailingOnly=T)
 null_file <- args[1]
-outpath <- args[2]
+poscon_file <- args[2]
+outpath <- args[3]
 
 # read in data
 d <- read.delim(null_file)
+poscon <- read.delim(poscon_file)
 
 # add column to compute clique_density
 d$clique_density <- d$num_cliques / d$sample_size
+d$type <- rep("DMSO", nrow(d))
+poscon$clique_density <- poscon$num_cliques / poscon$sample_size
+poscon$type <- rep("Bio", nrow(poscon))
+#D <- rbind(d, poscon)
 
 # make histogram of clique_density for a series
 # of sizes and score thresholds
 # sizes <- c(10, 20, 30, 40, 50)
 sizes <- c(60, 70, 80, 90, 100)
 scores <- c(90, 92, 94, 96, 98)
-dsub <- droplevels(subset(d, sample_size %in% sizes & score %in% scores))
+dsub <- droplevels(subset(D, sample_size %in% sizes & score %in% scores))
 
-p <- ggplot(dsub, aes(x=clique_density)) + theme_bw()
+p <- ggplot(dsub[dsub$type=="DMSO", ], aes(x=clique_density)) + theme_bw()
 p <- p + geom_histogram()
+p <- p + geom_rug(data=dsub[dsub$type=="Bio", ], aes(x=clique_density), size=2, col="firebrick")
 p <- p + facet_grid(sample_size ~ score)
+# set the x limits to be something reasonable
+p <- p + xlim(0, 3000)
 ggsave(paste(outpath, "null_clique_hist.png", sep="/"), height=7, width=7)
